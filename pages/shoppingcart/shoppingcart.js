9225883a7b9ac0529fb9ad00177262bb20753423
Unknown <zhangyu199946@gmail.com>
2018-08-21 17:07:09 +0800

    主页

--- pages/shoppingcart/shoppingcart.js
+++ pages/shoppingcart/shoppingcart.js
@@ -1,33 +1,11 @@
-// pages/found/found.js
+// pages/shoppingcart/shoppingcart.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    inputShowed: false,
-    inputVal: ""
-  },
-  showInput: function () {
-    this.setData({
-      inputShowed: true
-    });
-  },
-  hideInput: function () {
-    this.setData({
-      inputVal: "",
-      inputShowed: false
-    });
-  },
-  clearInput: function () {
-    this.setData({
-      inputVal: ""
-    });
-  },
-  inputTyping: function (e) {
-    this.setData({
-      inputVal: e.detail.value
-    });
+
   },
 
   /**
