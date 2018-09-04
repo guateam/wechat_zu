bd3eb06ad2e7fba8d78e792fda0dbec7e40c0e81
Unknown <zhangyu199946@gmail.com>
2018-09-04 11:33:58 +0800

    技师界面数据填充

---
+++ php/getfeedback.php
@@ -0,0 +1,48 @@
+<?php
+function getfeedback($id){
+    $serivce=get('service_order','job_number',$id);
+    $data=[];
+    foreach($serivce as $value){
+        $rate=get('rate','order_id',$value['order_id']);
+        if($rate){
+            $rate=$rate[0];
+            $order=get('consumed_order','order_id',$value['order_id']);
+            if($order){
+                $order=$order[0];
+                $user=get('customer','ID',$order['user_id']);
+                if($user){
+                    $user=$user[0];
+                    $head='../photo/defult.jpg';
+                    if($user['head']!=''){
+                        $head=$user['head'];
+                    }
+                    $price=0;
+                    if($value['servicetype']==1){
+                        $serivcetype=get('service_type','ID',$rate['service_id']);
+                        if($serivcetype){
+                            $price=$serivcetype[0]['price'];
+                        }
+                    }else if($value['servicetype']==2){
+                        $serivcetype=get('item_type','ID',$rate['service_id']);
+                        if($serivcetype){
+                            $price=$serivcetype[0]['price'];
+                        }
+                    }else{
+                        $price=$value['price'];
+                    }
+                    
+                    $item=[
+                        'name'=>$user['name'],
+                        'head'=>'border-radius: 50%;background-color: #ffcc99;background-image: url("'.$head.'");background-size:cover',
+                        'rate'=>$rate['score'],
+                        'text'=>$rate['comment'],
+                        'serviceid'=>$value['oder_id'],
+                        'price'=>$price
+                    ];
+                    array_push($data,$item);
+                }
+            }
+        }
+    }
+    return $data;
+}
