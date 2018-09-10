fbd33d6dddfbff74cef9078d26851196c6f4ef61
yun_cn <zzwyxl@163.com>
2018-09-10 20:21:51 +0800

    两个页面基本完成

---
+++ php/getorderdetail.php
@@ -0,0 +1,38 @@
+<?php
+require("database.php");
+$order_id = $_POST['id'];
+$co = get("consumed_order","order_id",$order_id);
+$so = get("service_order","order_id",$order_id);
+$info = [];
+foreach($so as $svod){
+    $tech = get("technician","job_number",$svod['job_number']);
+    $svs = get("service_type","ID",$svod['item_id']);
+    $appo = null;
+    if($co[0]['state']==1){
+        $appo = get("appointment",'order_id',$order_id);
+    }
+    if($tech && $svs){
+        array_push($info,[
+            "technician_name"=>$tech[0]['name'],
+            "job_number"=>$tech[0]['job_number'],
+            "service_name"=>$svs[0]['name'],
+            "price"=>$svs[0]['price']*$svs[0]['discount']*0.01*0.01,
+            "time"=>$co[0]['generated_time'],
+            "pay_way"=>$co[0]['payment_method'],
+            "user_id"=>$co[0]['user_id'],
+            "appo"=>$appo
+        ]);
+    }else{
+        array_push($info,[
+            "technician_name"=>"",
+            "job_number"=>"",
+            "service_name"=>"",
+            "price"=>0,
+            "time"=>"",
+            "pay_way"=>"",
+            "user_id"=>"",
+            "appo"=>$appo
+        ]);
+    }
+}
+echo json_encode($info);
