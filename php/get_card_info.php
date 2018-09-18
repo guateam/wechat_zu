1385b28407b762e20d286c5a927dd2053654d825
yun_cn <zzwyxl@163.com>
2018-09-18 15:39:08 +0800

    问题4解决

--- php/get_card_info.php
+++ php/get_card_info.php
@@ -1,11 +1,16 @@
 <?php
 require("database.php");
 $user_id = $_POST['user_id'];
+$is_vip = get("customer",'ID',$user_id);
+$is_vip = $is_vip[0]['level']==0?false:true;
 $result = get("recharge_info",'user_id',$user_id);
-if($result){
+if($result && $is_vip){
     $result[0]['lvup']/=100;
     $result[0]['total_recharge']/=100;
     echo json_encode(['status'=>1,'data'=>$result[0]]);
 }else{
-    echo json_encode(['status'=>0]);
+    $first_lv = get("vip_information",'level',1);
+    echo json_encode(['status'=>0,'data'=>[
+        'next_lv'=>$first_lv[0]['name'],
+    ]]);
 }
