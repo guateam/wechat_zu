d267d439693981d3b61ad4b4aadc0a0f06b02f41
yun_cn <zzwyxl@163.com>
2018-09-20 08:19:26 +0800

    会员卡页面头像实装

--- php/get_card_info.php
+++ php/get_card_info.php
@@ -2,15 +2,16 @@
 require("database.php");
 $user_id = $_POST['user_id'];
 $is_vip = get("customer",'ID',$user_id);
+$head = $is_vip[0]['head'];
 $is_vip = $is_vip[0]['level']==0?false:true;
 $result = get("recharge_info",'user_id',$user_id);
 if($result && $is_vip){
     $result[0]['lvup']/=100;
     $result[0]['total_recharge']/=100;
-    echo json_encode(['status'=>1,'data'=>$result[0]]);
+    echo json_encode(['status'=>1,'data'=>$result[0],'head'=>$head]);
 }else{
     $first_lv = get("vip_information",'level',1);
-    echo json_encode(['status'=>0,'data'=>[
+    echo json_encode(['status'=>0,'head'=>$head,'data'=>[
         'next_lv'=>$first_lv[0]['name'],
     ]]);
 }
