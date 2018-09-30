b31b0e2f6a900afdb3eaaa3a7b129a4af30ada1e
yun_cn <zzwyxl@163.com>
2018-09-30 17:07:00 +0800

    有关视图和sql函数的内容全部删除

--- php/get_card_info.php
+++ php/get_card_info.php
@@ -4,11 +4,39 @@ $user_id = $_POST['user_id'];
 $is_vip = get("customer",'ID',$user_id);
 $head = $is_vip[0]['head'];
 $is_vip = $is_vip[0]['level']==0?false:true;
-$result = get("recharge_info",'user_id',$user_id);
+
+$str = "select `recharge_record`.`user_id` AS `user_id`,sum(`recharge_record`.`charge`) AS `total_charge` from `recharge_record` group by `recharge_record`.`user_id`";
+$result = sql_str($str);
+$data=[];
+foreach($result as $row){
+    if($row['user_id'] == $user_id){
+        $result = $row;
+        $str = "SELECT `extra_duration` FROM vip_information WHERE necessary_charge_to_level_up > ".$result['total_charge']." LIMIT 1";
+        $extradura = (sql_str($str))[0]['extra_duration'];
+
+        $str = "SELECT group_concat(`necessary_charge_to_level_up` SEPARATOR ',')  AS str FROM vip_information WHERE necessary_charge_to_level_up > ".$result['total_charge']." LIMIT 1 ;";
+        $exp = (int)(explode(',',(sql_str($str))[0]['str']))[0];
+        if($exp)$exp = $exp -  $result['total_charge'];
+        else $exp = 0;
+
+        $str = "SELECT group_concat(`name` SEPARATOR ',') AS str FROM vip_information WHERE necessary_charge_to_level_up > ".$result['total_charge'];
+        $lv_list = (explode(',',(sql_str($str))[0]['str']));
+        $nowlv = $lv_list[0];
+        $nxlv = "无";
+        if(count($lv_list)>1) $nxlv = $lv_list[1];
+
+        $str = "SELECT `discount_ratio` FROM vip_information WHERE necessary_charge_to_level_up > ".$result['total_charge']." LIMIT 1 ";
+        $discount = (sql_str($str))[0]['discount_ratio'];
+
+        $result = array_merge($result,["extradura"=>$extradura,"exp"=>$exp,'nowlv'=>$nowlv,'nxlv'=>$nxlv,'discount'=>$discount]);
+        break;
+    }
+}
+    
 if($result && $is_vip){
-    $result[0]['lvup']/=100;
-    $result[0]['total_recharge']/=100;
-    echo json_encode(['status'=>1,'data'=>$result[0],'head'=>$head]);
+    $result['exp']/=100;
+    $result['total_charge']/=100;
+    echo json_encode(['status'=>1,'data'=>$result,'head'=>$head]);
 }else{
     $first_lv = get("vip_information",'level',1);
     echo json_encode(['status'=>0,'head'=>$head,'data'=>[
--- php/getrate.php
+++ php/getrate.php
@@ -1,7 +1,32 @@
 <?php
 require("database.php");
 $order_id = $_POST['id'];
-$rate = get("rate_info","order_id",$order_id);
+$str = "SELECT
+`rate`.`order_id` AS `order_id`,
+`rate`.`score` AS `score`,
+`rate`.`comment` AS `comment`,
+`rate`.`bad` AS `bad`,
+`service_type`.`name` AS `name`,
+`service_order`.`job_number` AS `job_number`,
+`consumed_order`.`generated_time` AS `generated_time`,
+`rate`.`customer_id` AS `customer_id` 
+FROM
+(
+    (
+        ( `rate` JOIN `service_type` ON ( ( `rate`.`service_id` = `service_type`.`ID` ) ) )
+        JOIN `service_order` ON ( ( `service_order`.`order_id` = `rate`.`order_id` ) ) 
+    )
+JOIN `consumed_order` ON ( ( `consumed_order`.`order_id` = `rate`.`order_id` ) ) 
+)";
+
+$rates = sql_str($str);
+$this_rate = [];
+foreach($rates as $rate) {
+ if($rate['order_id'] == $order_id){
+     array_push($this_rate,$rate);
+ }
+}
+
 if($rate){
     echo json_encode(['status'=>1,'data'=>$rate]);
 }else{
