2108c2319bee9efe991e24133b174c76f1f7c502
yun_cn <zzwyxl@163.com>
2018-10-11 19:18:27 +0800

    修改确认订单的接口

--- php/addco.php
+++ php/addco.php
@@ -1,10 +1,11 @@
 <?php
 require("database.php");
+$id = $_POST['id'];
 $phone = $_POST['phone'];
 $job_number = $_POST['job_number'];
 $people_num  = $_POST['people_num'];
 $item_id = $_POST['item_id'];
-$user = get("customer","phone_number",$phone);
+$user = get("customer","openid",$id);
 $dict=['1','2','3','4','5','6','7','8','9','0'];
 $rnd = "";
 for($i=0;$i<3;$i++){
@@ -13,7 +14,7 @@ for($i=0;$i<3;$i++){
 }
 if($user){
     $time = date("Y md").$rnd;
-    add("consumed_order",[["order_id",$time],['user_id',$user[0]['ID']],['state',1]]);
+    add("consumed_order",[["order_id",$time],['user_id',$user[0]['ID']],['state',1],['contact_phone',$phone]]);
     foreach($item_id as $id){
         add("service_order",[['order_id',$time],['service_type',1],["item_id",$id],["job_number",$job_number]]);
     }
