a903181c8266ee2c338aed2ad7d2eaa81c652d97
yun_cn <zzwyxl@163.com>
2018-10-16 22:28:08 +0800

    客服电话起效，

--- php/getshoptime.php
+++ php/getshoptime.php
@@ -6,6 +6,7 @@ echo json_encode(
         'status'=>1,
         'data'=>[
             'name'=>$shop[0]['name'],
+            'tel'=>$shop[0]['phone'],
             'position'=>$shop[0]['position'],
             'location'=>'../html/map.html?location='.$shop[0]['position'],
             'open_time'=>$shop[0]['open_time'],
