c7997a214712c5febffda932f3752fff56074fc5
yun_cn <zzwyxl@163.com>
2018-10-28 19:58:55 +0800

    删除cookie

--- php/get_tech_by_skill.php
+++ php/get_tech_by_skill.php
@@ -4,7 +4,12 @@ $id =$_POST['service_id'];
 $numbers = [];
 //获取服务
 $service_name = sql_str("select name,have_level from service_type where `ID` = '$id'");
-$tech_info = sql_str("select A.job_number,A.photo,B.level,C.name from technician A,skill B,skill_level C where A.job_number = B.job_number and B.service_id = 4 and C.ID = B.level");
+$tech_info = sql_str("select A.job_number,A.photo,B.level,C.name from technician A,skill B,skill_level C where A.job_number = B.job_number and B.service_id = '$id' and C.ID = B.level");
+$no_level = sql_str("select A.job_number,A.photo,B.level from technician A,skill B where A.job_number = B.job_number and B.service_id = '$id' and B.level = 0");
+for($i=0;$i<count($no_level);$i++){
+    $no_level[$i] =  array_merge($no_level[$i],['name'=>""]);
+}
+$tech_info = array_merge($tech_info,$no_level);
 // $friend_circle = sql_str("select ")
 foreach($tech_info as $idx => $tc){
     $job_number = $tc['job_number'];
