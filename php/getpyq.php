d3e016a3cc0f80b1f229907f22f608e1350c376e
yun_cn <zzwyxl@163.com>
2018-11-07 15:55:25 +0800

    点击播放和暂停

--- php/getpyq.php
+++ php/getpyq.php
@@ -33,7 +33,7 @@ foreach($datas as $data)
             $result = get("technician_video",'ID',$videoid);
             if($result)
             {
-                $datas[$i]['video'][$j] = $result[0]['dir'];
+                $datas[$i]['video'][$j] = ['src'=>$result[0]['dir'],'play'=>false];
             }
             $j++;
         }
@@ -57,7 +57,28 @@ foreach($datas as $data)
     $tm1 = $datas[$i]['date'];
     $tm2 = time();
     $gap = $tm2-$tm1;
-
+    //转换成文字表示
+    if ($gap < 60) $gap = $gap."秒前";
+    else {
+        $gap /= 60;
+        if ($gap < 60) $gap = floor($gap)."分钟前";
+        else {
+            $gap /= 60;
+            if ($gap < 24) $gap = floor($gap)."小时前";
+            else {
+                $gap /= 24;
+                if ($gap <= 30) $gap = floor($gap)."天前";
+                else {
+                    $gap /= 30;
+                    if ($gap <= 12) $gap = floor($gap)."月前";
+                    else {
+                        $gap /= 12;
+                        if ($gap >= 1) $gap =floor($gap)."年前";
+                    }
+                }
+            }
+        }
+    }
     $datas[$i]['date']=$gap;
     $i++;
 }
