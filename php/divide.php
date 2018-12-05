d7e1c9f6d7eaecdd3931e151dca813b62fb5262f
yun_cn <zzwyxl@163.com>
2018-12-05 12:39:33 +0800

    时间选择逻辑修改

--- php/divide.php
+++ php/divide.php
@@ -4,10 +4,10 @@ $block = $_POST['block'];
 $idx = $_POST['idx'];
 $idx2 = $_POST['idx2'];
 $time_block_length = $_POST['length'];
-$day = intval($_POST['day']);
+//$day = intval($_POST['day']);
 
 $leap=1800;
-$start = intval($block['time'])+$day-strtotime(date('Y-m-d',time()));
+$start = intval($block['time']);
 //新的divide_line
 $tp = [];
 $divide_block = [];
@@ -17,7 +17,7 @@ $first_allow = 1000000;
 for ($i = $start,$j=0; $i < $start + $leap; $i += 300,$j++) {
     $si = date("Y-m-d H:i:s",$i);
 
-    $p = $i+$day-(3600*24);
+    $p = $i;//+$day-(3600*24);
     $allow = time()>$i?0:1;
     if($allow && $j < $first_allow )$first_allow = $j;
 
