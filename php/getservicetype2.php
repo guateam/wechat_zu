5abd42df867bfc7a9aa2343095026445c431be5d
wilson <thkdragon@cjlu.edu.cn>
2019-01-15 21:23:43 +0800

    添加一个php，为了显示宽的图片

---
+++ php/getservicetype2.php
@@ -0,0 +1,24 @@
+<?php
+function getservicetype2($id)
+{
+    $servicetype=get('service_type');
+    $data=[];
+    foreach($servicetype as $value)
+	{
+        $item=[
+            'id'=>$value['ID'],
+            'show'=>$value['index_show'],
+            'procedure'=>$value['procedure'],
+            'attention'=>$value['attention'],
+            'benefit'=>$value['benefit'],
+            'name'=>$value['name'],
+            'time'=>$value['duration'],
+            'price'=>$value['price']/100.0,
+            'img'=>$value['image2'],
+            'belong_to'=>$value['belong_to'],
+            'market_price'=>$value['market_price']/100.0
+        ];
+        array_push($data,$item);
+    }
+    return ['status'=>1,'data'=>$data];
+}
