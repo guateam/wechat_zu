31b37eb3abc4d6095cd2f6d0c7813c19507cd17f
yun_cn <zzwyxl@163.com>
2019-02-19 10:54:10 +0800

    历史订单页面

--- php/getbrieforder.php
+++ php/getbrieforder.php
@@ -2,10 +2,18 @@
 require("database.php");
 $id = $_POST['openid'];
 //获取所有消费订单
-$nopay_order = sql_str("select * from `consumed_order` where `state`=1 and `user_id`='$id' ORDER BY `generated_time` DESC");
-$pay_order = sql_str("select * from `consumed_order` where `state`=4 and `user_id`='$id' ORDER BY `generated_time` DESC");
-$complete_order = sql_str("select * from `consumed_order` where `state`=5 and `user_id`='$id' ORDER BY `generated_time` DESC");
 $all_order = sql_str("select * from `consumed_order` where `user_id`='$id' ORDER BY `generated_time` DESC");
+//待消费订单
+$nostart_order = sql_str("select * from `consumed_order` where `state`=1 and `user_id`='$id' ORDER BY `generated_time` DESC");
+//服务中订单
+$doing_order =  sql_str("select * from `consumed_order` where `state`=2 and `user_id`='$id' ORDER BY `generated_time` DESC");
+//待支付订单
+$nopay_order = sql_str("select * from `consumed_order` where `state`=3 and `user_id`='$id' ORDER BY `generated_time` DESC");
+//待评价订单
+$nojudge_order = sql_str("select * from `consumed_order` where `state`=4 and `user_id`='$id' ORDER BY `generated_time` DESC");
+//评价完成订单
+$complete_order = sql_str("select * from `consumed_order` where `state`=5 and `user_id`='$id' ORDER BY `generated_time` DESC");
+
 //$consumed_order = get("consumed_order");
 // foreach($consumed_order as $co)
 // {
@@ -50,4 +58,6 @@ $all_order = sql_str("select * from `consumed_order` where `user_id`='$id' ORDER
 //     }
 // }
 
-echo json_encode(['nopay'=>$nopay_order,'pay'=>$pay_order,'com'=>$complete_order,'allorder'=>$all_order]);
+echo json_encode(['nostart'=.$nostart_order,'doing'=>$doing_order,
+                'nopay'=>$nopay_order,'nojudgep'=>$nojudge_order,
+                'complete'=>$complete_order,'allorder'=>$all_order]);
